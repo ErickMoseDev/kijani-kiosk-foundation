19af418651f15e0d769039b1fc5a7a6fb8d3e164
Erick Mong'are <erickmongaremose@gmail.com>
2026-05-05 07:29:14 +0300

    fix: eslint errors

--- week5/apis-fetch-lab/eslint.config.mjs
+++ week5/apis-fetch-lab/eslint.config.mjs
@@ -1,14 +1,23 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 const customGlobals = {
-  ...globals.browser,
-  module: 'readonly',
+	...globals.browser,
+	module: 'readonly',
 };
 
 export default [
-  {
-    languageOptions: { globals: customGlobals },
-  },
-  pluginJs.configs.recommended,
+	{
+		languageOptions: { globals: customGlobals },
+	},
+	pluginJs.configs.recommended,
+	{
+		files: ['test/**/*.js'],
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.jest,
+			},
+		},
+	},
 ];
